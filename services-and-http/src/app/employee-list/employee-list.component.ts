721619a6b2e566f0a37336151915681c9d3c20f8
tejaskul29496 <43577458+tejaskul29496@users.noreply.github.com>
2019-05-09 11:55:44 +0530

    Added services and http

---
+++ services-and-http/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './../employee.service';
+
+@Component({
+  selector: 'employee-list',
+  templateUrl: 'employee-list.component.html',
+  styles: []
+})
+export class EmployeeListComponent implements OnInit {
+
+  public employees = [];
+  public errorMsg;
+  constructor(private _employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this._employeeService.getEmployees()
+      .subscribe(data => this.employees = data,
+            error => this.errorMsg = error);
+  }
+
+
+
+}
